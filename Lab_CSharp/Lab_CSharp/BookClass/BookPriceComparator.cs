0c7b94a3e40ea5007f71127efacaeff29fab35a1
Vladislav Mironchuk <vlad.mironchuk.02@gmail.com>
2021-12-07 14:19:00 +0300

    add: adf full project

---
+++ Lab_CSharp/Lab_CSharp/BookClass/BookPriceComparator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Lab_CSharp.BookClass
+{
+    /// <summary>Book Price Comparator</summary>
+    public class BookPriceComparator:IComparer<Book>
+    {
+        /// <summary>Compare two books by price</summary>
+        /// <returns>int</returns>
+        public int Compare(Book firstBook, Book secondBook)
+        {
+            if (firstBook == null && secondBook == null)
+            {
+                return 0;
+            }
+            
+            if (firstBook == null)
+            {
+                return -1;
+            }
+
+            if (secondBook == null)
+            {
+                return 1;
+            }
+
+            return firstBook.Price.CompareTo(secondBook.Price);
+        }
+    }
+}
